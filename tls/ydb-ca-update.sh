8129f50d4a2562ca58e018511e0f2fc29b24ae1e
Maksim Zinal <maxzinal@yandex.ru>
2022-10-18 16:08:33 +0300

    TLS certificate generation for YDB

---
+++ tls/ydb-ca-update.sh
@@ -0,0 +1,121 @@
+#! /bin/sh
+
+set -e
+set +u
+
+[ -d CA ] || mkdir CA
+cd CA
+
+[ -d secure ] || mkdir secure
+[ -d certs ] || mkdir certs
+[ -d nodes ] || mkdir nodes
+
+if [ ! -f ca.cnf ]; then
+    echo "** Generating CA configuration file"
+cat >ca.cnf <<EOF
+[ ca ]
+default_ca = CA_default
+
+[ CA_default ]
+default_days = 365
+database = index.txt
+serial = serial.txt
+default_md = sha256
+copy_extensions = copy
+unique_subject = no
+
+[ req ]
+prompt=no
+distinguished_name = distinguished_name
+x509_extensions = extensions
+
+[ distinguished_name ]
+organizationName = YDB
+commonName = YDB CA
+
+[ extensions ]
+keyUsage = critical,digitalSignature,nonRepudiation,keyEncipherment,keyCertSign
+basicConstraints = critical,CA:true,pathlen:1
+
+[ signing_policy ]
+organizationName = supplied
+commonName = optional
+
+[ signing_node_req ]
+keyUsage = critical,digitalSignature,keyEncipherment
+extendedKeyUsage = serverAuth,clientAuth
+
+# Used to sign client certificates.
+[ signing_client_req ]
+keyUsage = critical,digitalSignature,keyEncipherment
+extendedKeyUsage = clientAuth
+EOF
+fi
+
+if [ ! -f secure/ca.key ]; then
+    echo "** Generating CA key"
+    openssl genrsa -out secure/ca.key 2048
+fi
+
+if [ ! -f certs/ca.crt ]; then
+    echo "** Generating CA certificate"
+    openssl req -new -x509 -config ca.cnf -key secure/ca.key -out certs/ca.crt -days 1830 -batch
+fi
+
+[ -f index.txt ] || touch index.txt
+[ -f serial.txt ] || (echo 01 >serial.txt)
+
+if [ ! -f ../ydb-ca-nodes.txt ]; then
+    echo "** Missing file ydb-ca-nodes.txt - EXIT"
+    exit 0
+fi
+
+make_node_conf() {
+    if [ ! -f nodes/"$1".cnf ]; then
+        echo "** Creating node configuration file for $1..."
+cat > nodes/"$1".cnf <<EOF
+# OpenSSL node configuration file
+[ req ]
+prompt=no
+distinguished_name = distinguished_name
+req_extensions = extensions
+
+[ distinguished_name ]
+organizationName = YDB
+
+[ extensions ]
+subjectAltName = DNS:$1
+EOF
+    fi
+}
+
+make_node_key() {
+    if [ ! -f nodes/"$1".key ]; then
+         echo "** Generating key for node $1..."
+         openssl genrsa -out nodes/"$1".key 2048
+    fi
+}
+
+make_node_csr() {
+    if [ ! -f nodes/"$1".csr ]; then
+        echo "** Generating CSR for node $1..."
+        openssl req -new -sha256 -config nodes/"$1".cnf -key nodes/"$1".key -out nodes/"$1".csr -batch
+    fi
+}
+
+make_node_cert() {
+    if [ ! -f nodes/"$1".crt ]; then
+        echo "** Generating certificate for node $1..."
+        openssl ca -config ca.cnf -keyfile secure/ca.key -cert certs/ca.crt -policy signing_policy \
+            -extensions signing_node_req -out nodes/"$1".crt -outdir nodes/ -in nodes/"$1".csr -batch
+    fi
+}
+
+cat ../ydb-ca-nodes.txt | while read node; do
+    if [ ! -z "$node" ]; then
+        make_node_conf "$node"
+        make_node_key "$node"
+        make_node_csr "$node"
+        make_node_cert "$node"
+    fi
+done

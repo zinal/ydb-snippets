d76e6fb89e4471d01e70110acba913fa477bec15
Maksim Zinal <mzinal@ru.ibm.com>
2022-11-23 22:23:44 +0300

    финальные мелочи для mirror-3-dc

--- yc-cluster-sample/mirror-3-dc/90-extra-dynamic.sh
+++ yc-cluster-sample/mirror-3-dc/90-extra-dynamic.sh
@@ -6,16 +6,16 @@
 echo "Retrieving public SSH keyfile ${keyfile_gw} from host ${host_gw}..."
 ssh ${host_gw} cat ${keyfile_gw} >keyfile.tmp
 
-for i in `seq 17 19`; do
-  vm_name="${host_base}-${i}"
-  vm_disk_boot="${host_base}-${i}-boot"
+for i in `seq 5 8`; do
+  vm_name="${host_base}-d${i}"
+  vm_disk_boot="${host_base}-d${i}-boot"
   yc compute instance create ${vm_name} --zone ${yc_zone} \
     --platform ${yc_platform} \
     --ssh-key keyfile.tmp \
     --create-boot-disk name=${vm_disk_boot},type=network-ssd-nonreplicated,size=93G,auto-delete=true \
     --network-settings type=software-accelerated \
     --network-interface subnet-name=${yc_subnet},dns-record-spec="{name=${vm_name}.ru-central1.internal.}" \
-    --memory 24G --cores 8 --async
+    --memory 24G --cores 12 --async
 done
 
 echo "Waiting for VMs to get ready..."
@@ -32,8 +32,8 @@ done
 echo "Validating network access..."
 while true; do
   num_fail=0
-  for i in `seq 17 19`; do
-    vm_name="${host_base}-${i}"
+  for i in `seq 5 8`; do
+    vm_name="${host_base}-d${i}"
     ZODAK_TEST=`ssh ${host_gw} ssh -o StrictHostKeyChecking=no yc-user@${vm_name} echo ZODAK 2>/dev/null`
     if [ "$ZODAK_TEST" == "ZODAK" ]; then
       echo "Host ${vm_name} is available."
@@ -51,15 +51,15 @@ while true; do
 done
 
 echo "Configuring host names and timezones..."
-for i in `seq 17 19`; do
-  vm_name="${host_base}-${i}"
+for i in `seq 5 8`; do
+  vm_name="${host_base}-d${i}"
   ssh ${host_gw} ssh yc-user@${vm_name} sudo hostnamectl set-hostname ${vm_name}
   ssh ${host_gw} ssh yc-user@${vm_name} sudo timedatectl set-timezone Europe/Moscow
 done
 
 echo "Creating YDB user and group..."
-for i in `seq 17 19`; do
-  vm_name="${host_base}-${i}"
+for i in `seq 5 8`; do
+  vm_name="${host_base}-d${i}"
   ssh ${host_gw} ssh yc-user@${vm_name} sudo groupadd ydb >/dev/null 2>&1
   ssh ${host_gw} ssh yc-user@${vm_name} sudo useradd ydb -g ydb >/dev/null 2>&1
   ssh ${host_gw} ssh yc-user@${vm_name} sudo usermod -aG disk ydb >/dev/null 2>&1
@@ -68,8 +68,8 @@ for i in `seq 17 19`; do
 done
 
 echo "Uploading ydbd compressed binary..."
-for i in `seq 17 19`; do
-  vm_name="${host_base}-${i}"
+for i in `seq 5 8`; do
+  vm_name="${host_base}-d${i}"
   ssh ${host_gw} ssh yc-user@${vm_name} mkdir ${WORKDIR}  >/dev/null 2>&1
   ssh ${host_gw} scp ${WORKDIR}/ydbd.xz yc-user@${vm_name}:${WORKDIR}/ydbd.xz
 done
@@ -85,8 +85,8 @@ scp ydbd-unpack.sh.tmp ${host_gw}:${WORKDIR}/ydbd-unpack.sh
 
 
 echo "Building installation directories..."
-for i in `seq 17 19`; do
-  vm_name="${host_base}-${i}"
+for i in `seq 5 8`; do
+  vm_name="${host_base}-d${i}"
   ssh ${host_gw} scp ${WORKDIR}/ydbd-unpack.sh yc-user@${vm_name}:${WORKDIR}/
   ssh ${host_gw} ssh yc-user@${vm_name} sudo mkdir -p -v /opt/ydb/bin
   ssh ${host_gw} ssh yc-user@${vm_name} sudo mkdir -p -v /opt/ydb/cfg
@@ -94,18 +94,18 @@ for i in `seq 17 19`; do
   ssh ${host_gw} ssh yc-user@${vm_name} sudo chmod aoug+x /opt/ydb/bin/ydbd
 done
 
-scp config-8nodes.yaml ydbd-storage.service ydbd-testdb.service ydb-deploy-configs.sh ${host_gw}:${WORKDIR}/
+scp config-3nodes.yaml ydbd-storage.service ydbd-testdb.service ydb-deploy-configs.sh ${host_gw}:${WORKDIR}/
 
 echo "Deploying configuration files..."
-for i in `seq 17 19`; do
-  vm_name="${host_base}-${i}"
-  ssh ${host_gw} scp ${WORKDIR}/config-8nodes.yaml ${WORKDIR}/ydbd-storage.service \
+for i in `seq 5 8`; do
+  vm_name="${host_base}-d${i}"
+  ssh ${host_gw} scp ${WORKDIR}/config-3nodes.yaml ${WORKDIR}/ydbd-storage.service \
     ${WORKDIR}/ydbd-testdb.service ${WORKDIR}/ydb-deploy-configs.sh yc-user@${vm_name}:${WORKDIR}/
   ssh ${host_gw} ssh yc-user@${vm_name} sudo bash ${WORKDIR}/ydb-deploy-configs.sh
 done
 
 echo "Starting YDB database nodes..."
-for i in `seq 17 19`; do
-  vm_name="${host_base}-${i}"
+for i in `seq 5 8`; do
+  vm_name="${host_base}-d${i}"
   ssh ${host_gw} ssh yc-user@${vm_name} sudo systemctl start ydbd-testdb
 done
--- yc-cluster-sample/mirror-3-dc/95-dynamic-stop.sh
+++ yc-cluster-sample/mirror-3-dc/95-dynamic-stop.sh
@@ -4,7 +4,7 @@
 . ./options.sh
 
 echo "Stopping YDB database nodes..."
-for i in `seq 1 8`; do
-  vm_name="${host_base}-${i}"
+for i in `seq 1 4`; do
+  vm_name="${host_base}-d${i}"
   ssh ${host_gw} ssh yc-user@${vm_name} sudo systemctl stop ydbd-testdb
 done

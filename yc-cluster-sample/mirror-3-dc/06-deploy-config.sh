1a0d191e2f36d2ff901674ee4d54564a2d2d0cb8
Maksim Zinal <mzinal@ru.ibm.com>
2022-11-23 21:16:40 +0300

    прогресс mirror-3-dc

--- yc-cluster-sample/mirror-3-dc/06-deploy-config.sh
+++ yc-cluster-sample/mirror-3-dc/06-deploy-config.sh
@@ -3,12 +3,21 @@
 
 . ./options.sh
 
-scp config-8nodes.yaml ydbd-storage.service ydbd-testdb.service ydb-deploy-configs.sh ${host_gw}:${WORKDIR}/
+scp config-3nodes.yaml ydbd-storage.service ydbd-testdb.service ydb-deploy-configs.sh ${host_gw}:${WORKDIR}/
 
-echo "Deploying configuration files..."
-for i in `seq 1 16`; do
-  vm_name="${host_base}-${i}"
-  ssh ${host_gw} scp ${WORKDIR}/config-8nodes.yaml ${WORKDIR}/ydbd-storage.service \
+deploy_config() {
+  vm_name="$1"
+  ssh ${host_gw} scp ${WORKDIR}/config-3nodes.yaml ${WORKDIR}/ydbd-storage.service \
     ${WORKDIR}/ydbd-testdb.service ${WORKDIR}/ydb-deploy-configs.sh yc-user@${vm_name}:${WORKDIR}/
   ssh ${host_gw} ssh yc-user@${vm_name} sudo bash ${WORKDIR}/ydb-deploy-configs.sh
+}
+
+echo "Deploying configuration files..."
+for i in `seq 1 3`; do
+  vm_name="${host_base}-s${i}"
+  deploy_config ${vm_name}
+done
+for i in `seq 1 4`; do
+  vm_name="${host_base}-d${i}"
+  deploy_config ${vm_name}
 done

189af7ac381a9d3116d739a0c7c663f40ddf8de9
Maksim Zinal <mzinal@ru.ibm.com>
2022-11-23 21:25:51 +0300

    mirror-3-dc прогресс

--- yc-cluster-sample/mirror-3-dc/08-blobstorage-init.sh
+++ yc-cluster-sample/mirror-3-dc/08-blobstorage-init.sh
@@ -3,9 +3,10 @@
 
 . ./options.sh
 
+vm_name="${host_base}-s1"
 scp ydb-initial-token.sh ${host_gw}:${WORKDIR}/
 scp ydb-init-blobstorage.sh ${host_gw}:${WORKDIR}/
-ssh ${host_gw} bash ${WORKDIR}/ydb-initial-token.sh "${host_base}-1"
-ssh ${host_gw} scp ydbd-token-file yc-user@"${host_base}-1":ydbd-token-file
-ssh ${host_gw} scp ${WORKDIR}/ydb-init-blobstorage.sh yc-user@"${host_base}-1":${WORKDIR}/
-ssh ${host_gw} ssh yc-user@"${host_base}-1" bash ${WORKDIR}/ydb-init-blobstorage.sh
+ssh ${host_gw} bash ${WORKDIR}/ydb-initial-token.sh ${vm_name}
+ssh ${host_gw} scp ydbd-token-file yc-user@"${vm_name}":ydbd-token-file
+ssh ${host_gw} scp ${WORKDIR}/ydb-init-blobstorage.sh yc-user@"${vm_name}":${WORKDIR}/
+ssh ${host_gw} ssh yc-user@"${vm_name}" bash ${WORKDIR}/ydb-init-blobstorage.sh
--- yc-cluster-sample/mirror-3-dc/09-db-create.sh
+++ yc-cluster-sample/mirror-3-dc/09-db-create.sh
@@ -3,6 +3,7 @@
 
 . ./options.sh
 
+vm_name="${host_base}-s1"
 scp ydb-create-db.sh ${host_gw}:${WORKDIR}/
-ssh ${host_gw} scp ${WORKDIR}/ydb-create-db.sh yc-user@"${host_base}-1":${WORKDIR}/
-ssh ${host_gw} ssh yc-user@"${host_base}-1" bash ${WORKDIR}/ydb-create-db.sh
+ssh ${host_gw} scp ${WORKDIR}/ydb-create-db.sh yc-user@"${vm_name}":${WORKDIR}/
+ssh ${host_gw} ssh yc-user@"${vm_name}" bash ${WORKDIR}/ydb-create-db.sh

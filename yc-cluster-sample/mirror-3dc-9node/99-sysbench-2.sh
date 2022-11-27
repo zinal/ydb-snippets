953ec2e24c877cbcc64f30022f32372e254d93d7
Maksim Zinal <mzinal@ru.ibm.com>
2022-11-27 23:15:23 +0300

    16-core sysbench results

--- yc-cluster-sample/mirror-3dc-9node/99-sysbench-2.sh
+++ yc-cluster-sample/mirror-3dc-9node/99-sysbench-2.sh
@@ -7,7 +7,7 @@ mkdir -p sysbench.out.tmp
 
 GrabSysbench() {
   vm_name="$1"
-  fname=sysbench.${vm_name}.8c.txt
+  fname=sysbench.${vm_name}.24c.txt
   echo "...from host ${vm_name}..."
   ssh ${host_gw} scp yc-user@${vm_name}:sysbench.out.txt ${WORKDIR}/${fname}
   scp ${host_gw}:${WORKDIR}/${fname} sysbench.out.tmp/

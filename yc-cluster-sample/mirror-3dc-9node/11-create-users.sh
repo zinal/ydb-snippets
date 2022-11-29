4a81b50f71f82325f899537e1a88e6177eaeb175
Maksim Zinal <mzinal@ru.ibm.com>
2022-11-29 11:00:40 +0300

    скрипты запуска и увеличения количества дисковых групп

--- yc-cluster-sample/mirror-3dc-9node/11-create-users.sh
+++ yc-cluster-sample/mirror-3dc-9node/11-create-users.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
+ydb -e grpc://ycydb-d1:2136 -d /Root/testdb --user root --no-password yql -s 'CREATE USER stroppy PASSWORD "passw0rd"'
+ydb -e grpc://ycydb-d1:2136 -d /Root/testdb --user root --no-password yql -s 'ALTER GROUP ADMINS ADD USER stroppy'
 ydb -e grpc://ycydb-d1:2136 -d /Root/testdb --user root --no-password yql -s 'ALTER USER root PASSWORD "passw0rd"'
-ydb -e grpc://ycydb-d1:2136 -d /Root/testdb --user root yql -s 'CREATE USER stroppy PASSWORD "passw0rd"'
-ydb -e grpc://ycydb-d1:2136 -d /Root/testdb --user root yql -s 'ALTER GROUP ADMINS ADD USER stroppy'

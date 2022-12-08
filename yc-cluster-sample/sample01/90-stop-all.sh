cb14aee9279c11b4c7576a06f78ec9cd9c059722
Maksim Zinal <mzinal@ru.ibm.com>
2022-12-08 18:22:57 +0300

    tls config

--- yc-cluster-sample/sample01/90-stop-all.sh
+++ yc-cluster-sample/sample01/90-stop-all.sh
@@ -7,5 +7,4 @@ echo "Stopping YDB nodes..."
 for i in `seq 1 ${ydb_static}`; do
   vm_name="${host_base}-s${i}"
   ssh ${host_gw} ssh yc-user@${vm_name} sudo systemctl stop ydbd-testdb
-  ssh ${host_gw} ssh yc-user@${vm_name} sudo systemctl stop ydbd-storage
 done

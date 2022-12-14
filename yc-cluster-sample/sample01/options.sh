b09dc8444b3d8a278bd2a6b766829b46c77f5d0b
Maksim Zinal <mzinal@ru.ibm.com>
2022-12-14 15:06:23 +0300

    full tls config

--- yc-cluster-sample/sample01/options.sh
+++ yc-cluster-sample/sample01/options.sh
@@ -9,17 +9,17 @@ keyfile_gw=.ssh/id_ecdsa.pub
 
 yc_zone=ru-central1-b
 yc_subnet=default-ru-central1-b
-yc_platform=standard-v3
+yc_platform=standard-v2
 yc_vm_image="image-folder-id=standard-images,image-family=ubuntu-2204-lts"
 yc_vm_cores=32
 yc_vm_mem=64
-yc_data_disk_size=186G
+yc_data_disk_size=279G
 
 ydb_static=9
-ydb_disk_count=3
+ydb_disk_count=1
 ydb_config=conf-9n-32c-tls.yaml
 ydb_tls=Y
-ydb_disk_groups=27
+ydb_disk_groups=9
 
 ydbd_mode=tar
 
--- yc-cluster-sample/sample01/ydb-deploy-configs.sh
+++ yc-cluster-sample/sample01/ydb-deploy-configs.sh
@@ -15,6 +15,7 @@ if [ "tls"=="$tls_mode" ]; then
   cp -v YdbWork/tls/ca.crt /opt/ydb/certs/
   cp -v YdbWork/tls/node.crt /opt/ydb/certs/
   cp -v YdbWork/tls/node.key /opt/ydb/certs/
+  cat YdbWork/tls/node.key YdbWork/tls/node.crt YdbWork/tls/ca.crt >/opt/ydb/certs/web.pem
   chown -R ydb:ydb /opt/ydb/certs
   chmod 700 /opt/ydb/certs
 fi

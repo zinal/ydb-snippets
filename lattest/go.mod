9f69f4c172ba1dc60a0a255c3bcd5265f9048f79
Maksim Zinal <maxzinal@yandex.ru>
2023-07-11 13:31:03 +0300

    lattest

--- lattest/go.mod
+++ lattest/go.mod
@@ -7,13 +7,18 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jonboulle/clockwork v0.4.0 // indirect
+	github.com/yandex-cloud/go-genproto v0.0.0-20230628143002-ac2343960883 // indirect
 	github.com/ydb-platform/ydb-go-genproto v0.0.0-20230710134411-552fdf84e4f1 // indirect
 	github.com/ydb-platform/ydb-go-sdk/v3 v3.48.5 // indirect
+	github.com/ydb-platform/ydb-go-yc v0.12.1 // indirect
+	github.com/ydb-platform/ydb-go-yc-metadata v0.6.1 // indirect
+	golang.org/x/exp v0.0.0-20230711023510-fffb14384f22 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
-	google.golang.org/genproto v0.0.0-20230706204954-ccb25ca9f130 // indirect
+	google.golang.org/genproto v0.0.0-20230710151506-e685fd7b542b // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230710151506-e685fd7b542b // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230710151506-e685fd7b542b // indirect
 	google.golang.org/grpc v1.56.2 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
--- lattest/main.go
+++ lattest/main.go
@@ -1,10 +1,207 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"golang.org/x/exp/rand"
+
+	"github.com/ydb-platform/ydb-go-sdk/v3"
+	"github.com/ydb-platform/ydb-go-sdk/v3/balancers"
+	"github.com/ydb-platform/ydb-go-sdk/v3/table"
+	"github.com/ydb-platform/ydb-go-sdk/v3/table/options"
+	"github.com/ydb-platform/ydb-go-sdk/v3/table/result"
+	"github.com/ydb-platform/ydb-go-sdk/v3/table/types"
+	yc "github.com/ydb-platform/ydb-go-yc"
 )
 
+type YdbAuthMode int
+
+const (
+	YdbAuthNone YdbAuthMode = iota
+	YdbAuthStatic
+	YdbAuthMeta
+	YdbAuthSaKey
+)
+
+var ydbAuthModeMap = map[string]YdbAuthMode{
+	"":       YdbAuthNone,
+	"none":   YdbAuthNone,
+	"static": YdbAuthStatic,
+	"meta":   YdbAuthMeta,
+	"sakey":  YdbAuthSaKey,
+}
+
+func ydbAuth(mode YdbAuthMode, q url.Values, u *url.URL) ydb.Option {
+	switch mode {
+	case YdbAuthStatic:
+		p, _ := u.User.Password()
+		return ydb.WithStaticCredentials(u.User.Username(), p)
+	case YdbAuthMeta:
+		return yc.WithMetadataCredentials()
+	case YdbAuthSaKey:
+		return yc.WithServiceAccountKeyFileCredentials(q.Get("saKeyFile"))
+	default:
+		return ydb.WithAnonymousCredentials()
+	}
+}
+
+func ydbConnect(addr string) (*ydb.Driver, error) {
+	u, err := url.Parse(addr)
+	if err != nil {
+		return nil, err
+	}
+	q := u.Query()
+	database := u.Path
+	if len(database) == 0 {
+		database = q.Get("database")
+	}
+	if !strings.HasPrefix(database, "/") {
+		database = "/" + database
+	}
+	tls := false
+	if strings.ToLower(u.Scheme) == "grpcs" {
+		tls = true
+	}
+	if q.Has("tls") {
+		tls, _ = strconv.ParseBool(q.Get("tls"))
+	}
+	serverless := false
+	if q.Has("serverless") {
+		serverless, _ = strconv.ParseBool(q.Get("serverless"))
+	}
+	var ydbUrl string
+	if tls {
+		ydbUrl = "grpcs://" + u.Host + database
+	} else {
+		ydbUrl = "grpc://" + u.Host + database
+	}
+	authModeStr := strings.ToLower(q.Get("authMode"))
+	authMode, authModeFound := ydbAuthModeMap[authModeStr]
+	if !authModeFound {
+		return nil, fmt.Errorf("unknown auth mode: %q", authModeStr)
+	}
+	certFile := q.Get("tlsCertFile")
+	if len(certFile) == 0 {
+		certFile = "/dev/null"
+	}
+
+	bconf := balancers.Default()
+	if serverless {
+		bconf = balancers.SingleConn()
+	}
+
+	var con *ydb.Driver
+	func() {
+		poolSize := 10
+		ydbContext, ctxCloseFn := context.WithTimeout(context.Background(), 10*time.Second)
+		defer ctxCloseFn()
+
+		con, err = ydb.Open(ydbContext, ydbUrl,
+			ydb.WithUserAgent("lattest"),
+			ydb.WithSessionPoolSizeLimit(poolSize),
+			ydb.WithBalancer(bconf),
+			ydb.WithSessionPoolIdleThreshold(time.Hour),
+			ydb.WithCertificatesFromFile(certFile),
+			ydbAuth(authMode, q, u))
+	}()
+	if err != nil {
+		return nil, err
+	}
+	return con, nil
+}
+
+func runTransaction(con *ydb.Driver, iter int) (total int, err error) {
+	ydbContext, ctxCloseFn := context.WithCancel(context.Background())
+	defer ctxCloseFn()
+	total = 0
+	err = con.Table().Do(
+		ydbContext, func(ctx context.Context, sess table.Session) error {
+			var txc *table.TransactionControl
+			var trans table.Transaction
+			var data result.Result
+			for i := 0; i < iter; i++ {
+				input := int32(rand.Intn(10000))
+				txcWork := txc
+				if txcWork == nil {
+					txcWork = table.SerializableReadWriteTxControl()
+				}
+				trans, data, err = sess.Execute(ctx, txcWork,
+					"DECLARE $p AS Int32; SELECT 1+$p AS out;",
+					table.NewQueryParameters(
+						table.ValueParam("$p", types.Int32Value(input)),
+					), options.WithKeepInCache(true),
+				)
+				if err != nil {
+					return err
+				}
+				if txc == nil {
+					txc = table.SerializableReadWriteTxControl(table.WithTx(trans))
+				}
+				if data.NextResultSet(ctx) {
+					if data.NextRow() {
+						var value int32
+						if err = data.Scan(&value); err != nil {
+							return err
+						}
+						if value != input+1 {
+							return fmt.Errorf("illegal query output, got %q, expected %q", value, input+1)
+						}
+					}
+				}
+				total = total + int(input)
+			}
+			return nil
+		}, table.WithIdempotent())
+	return total, err
+}
+
 func main() {
+	addr := "grpc://localhost:2136/local"
+	if len(os.Args) > 1 {
+		addr = os.Args[1]
+	}
+
 	fmt.Println("Starting up...")
+	ydbContext, ctxCloseFn := context.WithCancel(context.Background())
+	defer ctxCloseFn()
+	connectStart := time.Now()
+	con, err := ydbConnect(addr)
+	connectFinish := time.Now()
+	if err != nil {
+		panic(err)
+	}
+	defer con.Close(ydbContext)
+	fmt.Println("Connected! Duraction = " + connectFinish.Sub(connectStart).String())
+
+	rand.Seed(uint64(time.Now().UnixNano()))
+
+	operStart := time.Now()
+
+	operTotal := 10000
+	operDone := 0
+	operIter := 0
+	for operTotal > operDone {
+		operStep := 3 + rand.Intn(8)
+		if operDone+operStep > operTotal {
+			operStep = operTotal - operDone
+		}
+		_, err = runTransaction(con, operStep)
+		if err != nil {
+			panic(err)
+		}
+		operDone += operStep
+		operIter += 1
+	}
+
+	operFinish := time.Now()
+
+	fmt.Printf("Processed! Duraction = %s, iterations = %d", operFinish.Sub(operStart).String(), operIter)
+
 	fmt.Println("Shutting down...")
 }

bab79e90b64275e1a559c567b7413e8be4e6bd2d
Maksim Zinal <mzinal@ru.ibm.com>
2023-09-19 20:54:34 +0300

    x

---
+++ yc-cluster-sample/vms/52-extra-host.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+# Создание виртуальных машин Yandex Cloud для работы кластера YDB.
+
+. ./options.sh
+
+set -u
+set +e
+
+ydb_nodes_begin=`echo "${ydb_nodes} + 1" | bc`
+ydb_nodes_end=`echo "${ydb_nodes} + ${ydb_nodes_extra}" | bc`
+
+. ./supp/host.sh
+
+# End Of File

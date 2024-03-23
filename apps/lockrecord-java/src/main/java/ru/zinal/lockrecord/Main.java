123403e7d9c91200277bb9b2cdb6aa5e2991df60
Maksim Zinal <mzinal@ru.ibm.com>
2024-03-23 15:52:25 +0300

    проверенный тест

--- apps/lockrecord-java/src/main/java/ru/zinal/lockrecord/Main.java
+++ apps/lockrecord-java/src/main/java/ru/zinal/lockrecord/Main.java
@@ -1,6 +1,8 @@
 package ru.zinal.lockrecord;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  *
@@ -12,6 +14,8 @@ public class Main {
 
     final YdbConnector yc;
     final YdbLockRecord locker;
+    final AtomicInteger owner = new AtomicInteger(-1);
+    final AtomicLong execCount = new AtomicLong(0);
 
     public static void main(String[] args) {
         try {
@@ -33,39 +37,62 @@ public class Main {
     void run() {
         startExecutors();
         waitTime();
+
+        LOG.info("Total workload executions within the lock: {}", execCount.get());
     }
 
     void startExecutors() {
+        LOG.info("Starting executors...");
         for (int i=0; i<50; ++i) {
             var t = new Thread(new Executor(i));
             t.setDaemon(true);
             t.start();
         }
+        LOG.info("All executors are running!");
     }
 
     void work(int i) {
+        pause();
         locker.lock();
+        owner.set(i);
         try {
             realWork(i);
         } finally {
+            owner.set(-1);
             locker.unlock();
         }
     }
 
     void realWork(int i) {
-        LOG.info("Work started: " + i);
+        LOG.debug("Work started: " + i);
+        validate(i);
+        pause();
+        validate(i);
+        pause();
+        validate(i);
+        LOG.debug("Work completed: " + i);
+        execCount.addAndGet(1L);
+    }
+
+    void pause() {
         try {
-            Thread.sleep(ThreadLocalRandom.current().nextLong(500L, 2000L));
+            Thread.sleep(ThreadLocalRandom.current().nextLong(10L, 50L));
         } catch(InterruptedException ix) {}
-        LOG.info("Work completed: " + i);
+    }
+
+    void validate(int i) {
+        int cur = owner.get();
+        if (i!=cur) {
+            LOG.warn("Expected owner {}, actual owner {}", i, cur);
+        }
     }
 
     void waitTime() {
-        LOG.info("Running for 10 minutes...");
-        final long tvFinish = System.currentTimeMillis() + (10L*60L*1000L);
+        LOG.info("Running for 5 minutes...");
+        final long tvFinish = System.currentTimeMillis() + (5L*60L*1000L);
         do {
             try { Thread.sleep(500L); } catch(InterruptedException ix) {}
-        } while (tvFinish < System.currentTimeMillis());
+        } while (tvFinish > System.currentTimeMillis());
         LOG.info("Completed, terminating...");
     }
 

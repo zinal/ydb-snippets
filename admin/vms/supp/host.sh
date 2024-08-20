4ffb7868e8681fc9beeed93b7ff51202c39e6366
SloNN <alex.dmitriev@gmail.com>
2024-08-20 19:57:29 +0300

    Update host.sh

--- admin/vms/supp/host.sh
+++ admin/vms/supp/host.sh
@@ -4,8 +4,8 @@
 echo "Configuring host names and timezones..."
 for i in `seq ${ydb_nodes_begin} ${ydb_nodes_end}`; do
   vm_name="${host_base}${i}"
-  ssh ${host_gw} ssh ${host_user}@${vm_name} sudo hostnamectl set-hostname ${vm_name}
-  ssh ${host_gw} ssh ${host_user}@${vm_name} sudo timedatectl set-timezone Europe/Moscow
+  ssh ${host_gw} ssh -o StrictHostKeyChecking=no ${host_user}@${vm_name} sudo hostnamectl set-hostname ${vm_name}
+  ssh ${host_gw} ssh -o StrictHostKeyChecking=no ${host_user}@${vm_name} sudo timedatectl set-timezone Europe/Moscow
 done
 
 wait

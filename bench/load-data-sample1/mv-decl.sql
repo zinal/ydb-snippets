52125646a69aee0c43516c1eea7c060af01a14ca
Max Zinal <zinal@ydb.tech>
2026-02-05 19:37:43 +0300

    extra mv

--- bench/load-data-sample1/mv-decl.sql
+++ bench/load-data-sample1/mv-decl.sql
@@ -4,7 +4,7 @@ CREATE ASYNC MATERIALIZED VIEW mv1 AS
       main.c1 AS c1,
       main.dt AS dt
   FROM `bigtab1` AS main
-  WHERE COMPUTE ON main #[ 3 = Digest::CityHash(main.id) % 100 ]#;
+  WHERE COMPUTE ON main.id #[ 3 = (Digest::CityHash(main.id) % 10) ]#;
 
 CREATE ASYNC MATERIALIZED VIEW mv2 AS
   SELECT
@@ -19,9 +19,30 @@ CREATE ASYNC MATERIALIZED VIEW mv2 AS
   FROM `bigtab1` AS main
   INNER JOIN `dict_doctype` AS dict
     ON main.dst_type = dict.code;
+  WHERE COMPUTE ON main.id #[ 0 = (Digest::CityHash(main.id) % 2) ]#;
+
+CREATE ASYNC MATERIALIZED VIEW mv3 AS
+  SELECT
+      main.id AS id,
+      main.value_dt AS value_dt,
+      main.sys_crt_dttm AS sys_crt_dttm,
+      main.sys_upd_dttm AS sys_upd_dttm,
+      main.src_part AS src_part,
+      main.dst_part AS dst_part,
+      main.dst_type AS dst_type,
+      dict.name AS dst_name
+  FROM `bigtab1` AS main
+  INNER JOIN `dict_doctype` AS dict
+    ON main.dst_type = dict.code;
+  WHERE COMPUTE ON main.id #[ 1 = (Digest::CityHash(main.id) % 2) ]#;
 
 CREATE ASYNC HANDLER h1 CONSUMER h1_consumer
   PROCESS mv1,
   PROCESS mv2,
+  INPUT bigtab1 CHANGEFEED mv AS STREAM,
+  INPUT dict_doctype CHANGEFEED mv AS BATCH;
+
+CREATE ASYNC HANDLER h2 CONSUMER h2_consumer
+  PROCESS mv3
   INPUT bigtab1 CHANGEFEED mv AS STREAM,
   INPUT dict_doctype CHANGEFEED mv AS BATCH;
--- bench/load-data-sample1/mv-tables.sql
+++ bench/load-data-sample1/mv-tables.sql
@@ -55,3 +55,29 @@ CREATE TABLE mv2 (
       'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
       'u', 'v', 'w', 'x', 'y', 'z')
 );
+
+CREATE TABLE mv3 (
+	id	Utf8 NOT NULL,
+	value_dt	Timestamp64,
+	sys_crt_dttm	Timestamp64,
+	sys_upd_dttm	Timestamp64,
+	src_part	Utf8,
+	dst_part	Utf8,
+    dst_type Utf8,
+    dst_name Utf8,
+    PRIMARY KEY(id)
+) WITH (
+    AUTO_PARTITIONING_BY_LOAD = ENABLED,
+    AUTO_PARTITIONING_BY_SIZE = ENABLED,
+    AUTO_PARTITIONING_PARTITION_SIZE_MB = 2048,
+    AUTO_PARTITIONING_MIN_PARTITIONS_COUNT = 100,
+    AUTO_PARTITIONING_MAX_PARTITIONS_COUNT = 200,
+    PARTITION_AT_KEYS = (
+      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+      'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+      'U', 'V', 'W', 'X', 'Y', 'Z',
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+      'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+      'u', 'v', 'w', 'x', 'y', 'z')
+);

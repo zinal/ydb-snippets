c27b61a92e112833f242a18e5463de4b5b68e45e
Max Zinal <zinal@ydb.tech>
2026-02-05 19:58:16 +0300

    safer aliases

--- bench/load-data-sample1/mv-decl.sql
+++ bench/load-data-sample1/mv-decl.sql
@@ -1,40 +1,40 @@
 CREATE ASYNC MATERIALIZED VIEW mv1 AS
   SELECT
-      main.id AS id,
-      main.c1 AS c1,
-      main.dt AS dt
-  FROM `bigtab1` AS main
-  WHERE COMPUTE ON main.id #[ 3 = (Digest::CityHash(main.id) % 10) ]#;
+      main0.id AS id,
+      main0.c1 AS c1,
+      main0.dt AS dt
+  FROM `bigtab1` AS main0
+  WHERE COMPUTE ON main0.id #[ 3 = (Digest::CityHash(main0.id) % 10) ]#;
 
 CREATE ASYNC MATERIALIZED VIEW mv2 AS
   SELECT
-      main.id AS id,
-      main.value_dt AS value_dt,
-      main.sys_crt_dttm AS sys_crt_dttm,
-      main.sys_upd_dttm AS sys_upd_dttm,
-      main.src_part AS src_part,
-      main.dst_part AS dst_part,
-      main.dst_type AS dst_type,
-      dict.name AS dst_name
-  FROM `bigtab1` AS main
-  INNER JOIN `dict_doctype` AS dict
-    ON main.dst_type = dict.code
-  WHERE COMPUTE ON main.id #[ 0 = (Digest::CityHash(main.id) % 2) ]#;
+      main1.id AS id,
+      main1.value_dt AS value_dt,
+      main1.sys_crt_dttm AS sys_crt_dttm,
+      main1.sys_upd_dttm AS sys_upd_dttm,
+      main1.src_part AS src_part,
+      main1.dst_part AS dst_part,
+      main1.dst_type AS dst_type,
+      dict1.name AS dst_name
+  FROM `bigtab1` AS main1
+  INNER JOIN `dict_doctype` AS dict1
+    ON main1.dst_type = dict1.code
+  WHERE COMPUTE ON main1.id #[ 0 = (Digest::CityHash(main1.id) % 2) ]#;
 
 CREATE ASYNC MATERIALIZED VIEW mv3 AS
   SELECT
-      main.id AS id,
-      main.value_dt AS value_dt,
-      main.sys_crt_dttm AS sys_crt_dttm,
-      main.sys_upd_dttm AS sys_upd_dttm,
-      main.src_part AS src_part,
-      main.dst_part AS dst_part,
-      main.dst_type AS dst_type,
-      dict.name AS dst_name
-  FROM `bigtab1` AS main
-  INNER JOIN `dict_doctype` AS dict
-    ON main.dst_type = dict.code
-  WHERE COMPUTE ON main.id #[ 1 = (Digest::CityHash(main.id) % 2) ]#;
+      main2.id AS id,
+      main2.value_dt AS value_dt,
+      main2.sys_crt_dttm AS sys_crt_dttm,
+      main2.sys_upd_dttm AS sys_upd_dttm,
+      main2.src_part AS src_part,
+      main2.dst_part AS dst_part,
+      main2.dst_type AS dst_type,
+      dict2.name AS dst_name
+  FROM `bigtab1` AS main2
+  INNER JOIN `dict_doctype` AS dict2
+    ON main2.dst_type = dict2.code
+  WHERE COMPUTE ON main2.id #[ 1 = (Digest::CityHash(main2.id) % 2) ]#;
 
 CREATE ASYNC HANDLER h1 CONSUMER h1_consumer
   PROCESS mv1,

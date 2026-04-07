f87139cb652e21f230806852e6b0c59cd443612b
Max Zinal <zinal@ydb.tech>
2026-04-07 18:32:07 +0300

    добавил чтение к bigwriter

--- apps/bigwriter-java/src/main/java/ydb/tech/samples/bigwriter/App.java
+++ apps/bigwriter-java/src/main/java/ydb/tech/samples/bigwriter/App.java
@@ -67,7 +67,7 @@ public class App implements Runnable, AutoCloseable {
     private void handleSelectResult(QueryResultPart part) {
         var rs = part.getResultSetReader();
         if (rs.next()) {
-            LOG.info("** Current records count: {}", rs.getColumn(0).getInt64());
+            LOG.info("** Current records count: {}", rs.getColumn(0).getUint64());
         }
     }
 
